0e5bb89704e6ce0631cc42243f1b8a0b1b4f02e6
Ruma <matteoh8@hotmail.it>
2023-08-10 10:07:15 +0200

    load completato

--- load.php
+++ load.php
@@ -20,6 +20,13 @@
 
         //* Controlla se il file è stato caricato senza errori
         if (isset($_FILES['file-caricato']) && $_FILES['file-caricato']['error'] == 0) {
+
+            //? Cosa contiene $_FILES['file-caricato']
+            echo '<pre>';
+            print_r($_FILES['file-caricato']);
+            echo '</pre><br>';
+
+            //* Raccolta informazioni
             $fileTmpPath = $_FILES['file-caricato']['tmp_name'];
             $fileName = $_FILES['file-caricato']['name'];
             $fileSize = $_FILES['file-caricato']['size'];
@@ -27,12 +34,25 @@
             $fileNameCmps = explode(".", $fileName);
             $fileExtension = strtolower(end($fileNameCmps));
 
+            //* Controllo estensioni consentite
+            $allowedfileExtensions = array('jpg', 'jpeg', 'png', 'pdf');
+            if (!in_array($fileExtension, $allowedfileExtensions)) {
+                echo 'Caricamento non riuscito. I tipi di file permessi sono: ' . implode(", ", $allowedfileExtensions);
+                exit;
+            }
+
             //* Pulire nome del file e aggiunge un timestamp univoco per prevenire i conflitti di nome
             $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
 
             //* Directory in cui il file verrà salvato
             $uploadFileDir = './uploaded_files/';
             $dest_path = $uploadFileDir . $newFileName;
+
+            //* Creare la directory se non esiste
+            /* if (!is_dir($uploadFileDir)) {
+                mkdir($uploadFileDir, 0777, true);
+            }
+            $dest_path = $uploadFileDir . $newFileName; */
 
             if (move_uploaded_file($fileTmpPath, $dest_path)) {
                 echo 'Il file è stato caricato con successo.';
