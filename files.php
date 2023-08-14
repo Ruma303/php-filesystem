af6195f3b6d24fc0bb67343eb9ee3623f795b41c
Ruma <matteoh8@hotmail.it>
2023-08-14 19:39:06 +0200

    mostrare tabella

--- files.php
+++ files.php
@@ -48,7 +48,7 @@
             $allowedMimeTypes = array('image/jpeg', 'image/png', 'application/pdf');
             if (!in_array($mime, $allowedMimeTypes)) {
                 echo 'Caricamento non riuscito. I tipi di file permessi sono: '
-                . implode(", ", $allowedMimeTypes);
+                    . implode(", ", $allowedMimeTypes);
                 exit;
             }
 
@@ -68,15 +68,53 @@
             $dest_path = $uploadFileDir . '/' . $newFileName;
 
             if (move_uploaded_file($fileTmpPath, $dest_path)) {
+                $file_info_path = 'file_info.txt';
+                if (!file_exists($file_info_path)) {
+                    touch($file_info_path);
+                }
+
+
+                file_put_contents($file_info_path, "$fileName,$newFileName\n", FILE_APPEND);
                 echo 'Il file è stato caricato con successo.';
             } else {
                 echo 'Si è verificato un errore durante lo spostamento del file nella directory di upload. Assicurati che la directory di upload sia scrivibile dal server web.';
+            }
+
+
+            //* Mostrare i file
+            if (is_dir($uploadFileDir) && ($files = scandir($uploadFileDir))) {
+                $files = array_diff($files, array('.', '..'));
+
+                if (count($files) > 0) {
+                    echo "<table>
+                            <tr>
+                                <th>Nome file</th>
+                                <th>Nome temporaneo</th>
+                            </tr>";
+
+                    $file_info_path = 'file_info.txt';
+
+                    if (file_exists($file_info_path)) {
+                        $file_info = file($file_info_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+                        foreach ($file_info as $info) {
+                            list($originalName, $uniqueName) = explode(',', $info);
+                            echo "<tr>
+                                    <td>$originalName</td>
+                                    <td>$uniqueName</td>
+                                </tr>";
+                        }
+                    }
+                    echo "</table>";
+                }
             }
         } else {
             echo 'Si è verificato un errore nel caricamento del file.
             <br>Errore: ' . $_FILES['file-caricato']['error'];
         }
     }
+
+
     ?>
 </body>
 
